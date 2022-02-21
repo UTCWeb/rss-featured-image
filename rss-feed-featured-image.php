611f8ed03d044ff388fa2ff4a2db6e5bd7c82ca6
Chris Gilligan <49878588+UTCGilligan@users.noreply.github.com>
2022-02-21 12:00:14 -0500

    Update rss-feed-featured-image.php
    Turn Jetpack Photon image processing off for RSS feeds that use media:content.

--- rss-feed-featured-image.php
+++ rss-feed-featured-image.php
@@ -3,7 +3,7 @@
  * Plugin Name:   RSS Feed Featured Image
  * Plugin URI:    http://www.utc.edu/
  * Description:   A plugin to customize the default RSS Feed to add media enclosures for featured image
- * Version:	  1.3.3
+ * Version:	  1.3.4
  * Author:        Chris Gilligan
  * Author URI:    https://chrisgilligan.com/
  */
@@ -28,12 +28,17 @@ add_filter( 'rss2_item', 'rssimproved_attached_images' );
 function rssimproved_attached_images() {
     global $post;
 
+    // turn Jetpack Photon image processing off so we can get the correct image
+    $photon_removed = '';
+    if ( class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'photon' ) ) { // check that we are, in fact, using Photon in the first place
+	    $photon_removed = remove_filter( 'image_downsize', array( Jetpack_Photon::instance(), 'filter_image_downsize' ) );
+    }
+
     // if post has a featured image, full size featured image
     if ( get_the_post_thumbnail() ): ?>
-
             <media:content url="<?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full'); echo $image[0]; ?>" />
-
-    <?php endif;
+    <?php
+    endif;
 
     // define attachments
     $attachments = get_posts( array(
@@ -63,4 +68,8 @@ function rssimproved_attached_images() {
         <media:content url="https://www.utc.edu/sites/default/files/2021-11/utc-memorial-gate.jpg" />
 
     <?php endif;
+    // turn Photon back on
+    if ( $photon_removed ) {
+	    add_filter( 'image_downsize', array( Jetpack_Photon::instance(), 'filter_image_downsize' ), 10, 3 );
+    }
 }
